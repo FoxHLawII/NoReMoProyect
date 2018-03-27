32a8994d056df9bdb1820c2a8854a4fb602ba981
FoxH LawII <jfrojas251@gmail.com>
2018-03-26 22:42:54 -0500

    Autenticación OAuth, con passport.js y modulación del index.js

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
+/*ANTES DE MODULAR EL APLICATIVO
+
 //Se importa el modulo express
 //Express recibe y procesa las peticiones HTTP
 const express = require("express");
+//Librería para hacer un poco más fácil la autenticación con google, facebook, spotify etc...
+const passport = require("passport");
+//La estrategia provee metodos para hacer aún más fácil la autenticación con cierto sistema
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+//Llaves o pass entorno dev
+const keys = require("./config/keys");
 //Se crea la aplicacion express
 const app = express();
 //Se crea un puerto que será dado por heroku o se declarará en un archivo aparte
-const PORT= process.env.PORT || 3000;
-
-app.get("/",(request, response) => {
-    response.send({ hi: 'There'});
-});
-
+const PORT = process.env.PORT || 3000;
+//.use registra que passport usará determinada estrategia con algunas opciones (Pueden ser varias)
+//clientID = config/keys.js (Esto no se sube a git)
+//clientSecret = config/keys.js (Esto tampoco)
+//callbackURL = una vez autorizado por el usuario re redirigirá al la URL
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientId,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+        console.log(accessToken);
+        console.log(profile);
+    }
+  )
+);
+//Pide el perfil y el email usando la estrategia de google registrada anteriarmente
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+//Una vez el usuario autoriza, el callback de la estrategia google se ejecuta
+app.get("/auth/google/callback", passport.authenticate("google"));
 app.listen(PORT);
+*/
+const express=require("express");
+const passportConfig=require("./services/passport");
+const authRoutes=require("./routes/auth");
+const PORT=process.env.PORT || 3000;
+
+const app=express();
+
+authRoutes(app);
+app.listen(PORT)
