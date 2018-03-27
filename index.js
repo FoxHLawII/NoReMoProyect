38db5c08d8bfe021463cf07c01b515586a878122
FoxH LawII <jfrojas251@gmail.com>
2018-03-27 15:21:36 -0500

    Se agregan scripts de ejecucion, y el cliente React por separado

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get(
 app.get("/auth/google/callback", passport.authenticate("google"));
 app.listen(PORT);
 */
-const PORT=process.env.PORT || 3000;
+const PORT=process.env.PORT || 6000;
 const express=require("express");
 const mongoose=require("mongoose");
 const cookieSession=require("cookie-session");
