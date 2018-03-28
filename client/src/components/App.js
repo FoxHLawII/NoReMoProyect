723f24b97af2b23b60fd31802bfebc1b8ecd8782
FoxH LawII <jfrojas251@gmail.com>
2018-03-27 20:54:03 -0500

    Configuración basica de React-router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 
+const Header = () => <h2>HEADER</h2>;
+const Dashboard = () => <h2>Dashboar</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+
+//BrowserRauter y como un ViewRouter, solo puede tener un hijo, 
+//y éste a su vez tendrá las rutas y los componentes que mostrará
 const App = () => {
-  return <div>HI THERE</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" component={Landing} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
