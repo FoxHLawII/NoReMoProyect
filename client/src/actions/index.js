37a43d0696a1d57d04c65da0cda380428859e45b
FoxH LawII <jfrojas251@gmail.com>
2018-03-28 10:40:25 -0500

    Refactorzación de peticion axios a AsyncAwait

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,8 +1,23 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
 
-const fetchUser = dispatcher => {
-  axios
-    .get("/api/current_user")
-    .then(response => dispatcher({ type: FETCH_USER, payload: response }));
-};
+// export const fetchUser = () => {
+//   return function(dispatcher) {
+//     axios.get("/api/current_user").then(response => {
+//       dispatcher({ type: FETCH_USER, payload: response });
+//     });
+//   };
+// };
+
+// export const fetchUser=()=>{
+//   return async function(dispatcher){
+//     let response=await axios.get("/api/current_user");
+//     dispatcher({type:FETCH_USER, payload: response});
+//   }
+// }
+
+export const fetchUser = () =>
+  async function(dispatcher) {
+    let response = await axios.get("/api/current_user");
+    dispatcher({ type: FETCH_USER, payload: response });
+  };
